eecf10b52f512568e0f0c429f1eeaf22fbbb1465
bini-i <binyam.sh1@gmail.com>
2021-01-05 18:11:57 +0300

    Add friendship migration and associations
    - friendship migration with status attribute and self join association to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,10 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  has_many :friendships, foreign_key: :inviter_id
+  has_many :invitees, through: :friendships
+
+  has_many :friendships, foreign_key: :invitee_id
+  has_many :inviters, through: :friendships
 end
