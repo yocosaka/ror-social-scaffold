8bcacd9465e77cc3139a959a601793ffad349728
bini-i <binyam.sh1@gmail.com>
2021-01-08 12:48:31 +0300

    - Add who_invited_me, who_i_invited through tables and friends method
    - who_invited_me association goes through invitation_i_got table
    - who_i_invited association goes through invitations_i_sent table
    - friends method looks for all friends of a given user

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,12 @@
 class Friendship < ApplicationRecord
-  belongs_to :user
-  belongs_to :friend, class_name: 'User'
+  belongs_to :inviter, class_name: 'User'
+  belongs_to :invitee, class_name: 'User'
 end
+
+# user1 = User.create(name: 'user1', email: 'user1@gmail.com', password: '123456');
+# user2 = User.create(name: 'user2', email: 'user2@gmail.com', password: '123456');
+# user3 = User.create(name: 'user3', email: 'user3@gmail.com', password: '123456');
+
+# user1 = User.first;
+# user2 = User.second;
+# user3 = User.last;
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,52 +10,73 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  # User has many friendships & inverse friendships as a friend
-  has_many :friendships # as a user
-  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: :friend_id # as a friend
+  has_many :invitations_i_got, foreign_key: :invitee_id, class_name: 'Friendship'
+  has_many :who_invited_me, through: :invitations_i_got, source: :inviter
 
-  # ============== NOTE START ==============
-  # What is friendship?
-  # => People who I sent requests including both accepted and not accepted yet
+  has_many :invitations_i_sent, foreign_key: :inviter_id, class_name: 'Friendship'
+  has_many :who_i_invited, through: :invitations_i_sent, source: :invitee
 
-  # What is inverse_friendship?
-  # => People who I recieve their requests including both accepted and not accepted yet
+  def approve_requests
 
-  # What is friends?
-  # => People both who I accepted their requests and who accepted my requests
-  # Note: It doesn't include in case that either user doesn't accept
-  # ============== NOTE END ==============
-
-  # Define User's friends =>
-  # => People both who I accepted their requests and who accepted my requests
+  end
+  
   def friends
-    # Collect the friend_ids if my request was accepted as a user
-    friends_array = friendships.map { |friendship| friendship.friend if friendship.accepted }
-    # Collect the user_ids if I accepted the other's request as a freind
-    friends_array.concat(inverse_friendships.map { |friendship| friendship.user if friendship.accepted })
-    # Exclude User myself
-    friends_array.compact
+    friends = invitations_i_got.map{|friendship| friendship.inviter if friendship.accepted}
+    friends.concat(invitations_i_sent.map{|friendship| friendship.invitee if friendship.accepted})
+    friends.compact
   end
 
-  # People who haven't accpeted my request yet
-  def pending_friends
-    friendships.map { |friendship| friendship.friend unless friendship.accepted }.compact
+  def requests_i_got
+
   end
 
-  # People who I haven't accepted their request yet
-  def pending_requests
-    inverse_friendships.map { |friendship| friendship.user unless friendship.accepted }.compact
-  end
 
-  # Change the status of pending_requests from false to TRUE in order to build our friendships
-  def accept_request(user)
-    friendship = inverse_friendships.find { |fs| fs.user == user }
-    friendship.accepted = true
-    friendship.save
-  end
+  # # User has many friendships & inverse friendships as a friend
+  # has_many :friendships # as a user
+  # has_many :inverse_friendships, class_name: 'Friendship', foreign_key: :friend_id # as a friend
 
-  # Check if we are already friends or not which means both of us were accepted each other's request
-  def friend?(user)
-    friends.include?(user)
-  end
+  # # ============== NOTE START ==============
+  # # What is friendship?
+  # # => People who I sent requests including both accepted and not accepted yet
+
+  # # What is inverse_friendship?
+  # # => People who I recieve their requests including both accepted and not accepted yet
+
+  # # What is friends?
+  # # => People both who I accepted their requests and who accepted my requests
+  # # Note: It doesn't include in case that either user doesn't accept
+  # # ============== NOTE END ==============
+
+  # # Define User's friends =>
+  # # => People both who I accepted their requests and who accepted my requests
+  # def friends
+  #   # Collect the friend_ids if my request was accepted as a user
+  #   friends_array = friendships.map { |friendship| friendship.friend if friendship.accepted }
+  #   # Collect the user_ids if I accepted the other's request as a freind
+  #   friends_array.concat(inverse_friendships.map { |friendship| friendship.user if friendship.accepted })
+  #   # Exclude User myself
+  #   friends_array.compact
+  # end
+
+  # # People who haven't accpeted my request yet
+  # def pending_friends
+  #   friendships.map { |friendship| friendship.friend unless friendship.accepted }.compact
+  # end
+
+  # # People who I haven't accepted their request yet
+  # def pending_requests
+  #   inverse_friendships.map { |friendship| friendship.user unless friendship.accepted }.compact
+  # end
+
+  # # Change the status of pending_requests from false to TRUE in order to build our friendships
+  # def accept_request(user)
+  #   friendship = inverse_friendships.find { |fs| fs.user == user }
+  #   friendship.accepted = true
+  #   friendship.save
+  # end
+
+  # # Check if we are already friends or not which means both of us were accepted each other's request
+  # def friend?(user)
+  #   friends.include?(user)
+  # end
 end
