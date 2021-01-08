03b6fc89cf47fabf4e6506fa2526b299cf25476a
yocosaka <sakayoco55@gmail.com>
2021-01-08 19:40:49 +0900

    Add methods in User model - Add approve_requests(user) - Add friends - Add requests_for_friendship(user) - Add pending_requests_i_sent - Add pending_requests_i_got - Add friend?(user)

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,20 +16,38 @@ class User < ApplicationRecord
   has_many :invitations_i_sent, foreign_key: :inviter_id, class_name: 'Friendship'
   has_many :who_i_invited, through: :invitations_i_sent, source: :invitee
 
-  def approve_requests
-
+  def approve_requests(user)
+    friend_to_be = invitations_i_got.where(inviter_id: user.id).first
+    friend_to_be.accepted = true
+    friend_to_be.save
   end
-  
+
   def friends
-    friends = invitations_i_got.map{|friendship| friendship.inviter if friendship.accepted}
-    friends.concat(invitations_i_sent.map{|friendship| friendship.invitee if friendship.accepted})
+    friends = invitations_i_got.map { |friendship| friendship.inviter if friendship.accepted }
+    friends.concat(invitations_i_sent.map { |friendship| friendship.invitee if friendship.accepted })
     friends.compact
   end
 
-  def requests_i_got
+  def requests_for_friendship(user)
+    return false if who_i_invited.include?(user) || who_invited_me.include?(user)
 
+    who_i_invited << user
   end
 
+  # People who haven't accepted my request yet
+  def pending_requests_i_sent
+    invitations_i_sent.map { |friendship| friendship.invitee unless friendship.accepted }
+  end
+
+  # People who I haven't accepted their request yet
+  def pending_requests_i_got
+    invitations_i_got.map { |friendship| friendship.inviter unless friendship.accepted }
+  end
+
+  # Check if we are already friends or not which means both of us were accepted each
+  def friend?(user)
+    friends.include?(user)
+  end
 
   # # User has many friendships & inverse friendships as a friend
   # has_many :friendships # as a user
@@ -58,7 +76,7 @@ class User < ApplicationRecord
   #   friends_array.compact
   # end
 
-  # # People who haven't accpeted my request yet
+  # # People who haven't accepted my request yet
   # def pending_friends
   #   friendships.map { |friendship| friendship.friend unless friendship.accepted }.compact
   # end
--- db/migrate/20210108052126_create_friendships.rb
+++ db/migrate/20210108052126_create_friendships.rb
@@ -2,10 +2,9 @@ class CreateFriendships < ActiveRecord::Migration[5.2]
   def change
     create_table :friendships do |t|
       t.boolean :accepted, default: false
-      t.references :invitee, foreign_key: {to_table: :users}
-      t.references :inviter, foreign_key: {to_table: :users}
+      t.references :invitee, foreign_key: { to_table: :users }
+      t.references :inviter, foreign_key: { to_table: :users }
       t.timestamps
     end
   end
 end
-
