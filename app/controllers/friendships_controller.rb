15e3501c5a13fbe9ae9920d134972fb1f33b5e5b
yocosaka <sakayoco55@gmail.com>
2021-01-13 01:01:29 +0900

    Fix linter errors

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,17 +1,18 @@
 class FriendshipsController < ApplicationController
-    def create
-        # puts "output** #{current_user.name}"
-        invitee = User.find(params[:invitee_id])
-        current_user.requests_for_friendship(invitee)
-        redirect_to user_path(params[:invitee_id])           
+  def create
+    # puts "output** #{current_user.name}"
+    invitee = User.find(params[:invitee_id])
+    current_user.requests_for_friendship(invitee)
+    redirect_to user_path(params[:invitee_id])
+  end
+
+  def update
+    case params[:status]
+    when 'accept'
+      current_user.approve_request(Friendship.find(params[:id]).inviter)
+    when 'reject'
+      current_user.reject_request(Friendship.find(params[:id]).inviter)
     end
-    
-    def update
-        if params[:status] == 'accept'
-            current_user.approve_request(Friendship.find(params[:id]).inviter)
-        elsif params[:status] == 'reject'
-            current_user.reject_request(Friendship.find(params[:id]).inviter)
-        end
-        redirect_to users_path           
-    end
+    redirect_to users_path
+  end
 end
